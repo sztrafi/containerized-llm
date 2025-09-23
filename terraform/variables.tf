2cb94a8971255cef67e42b8c748734bab1375938
Filip Sztramski <sztrafi@Filips-MacBook-Pro.local>
2025-09-23 11:27:58 +0200

    tf.variables

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region to deploy resources"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  default     = "ai-api-rg"
+}
